7e6ec0bd9205ad4eb871df4bb1427c32c4ae05c3
M. Wahid Alqorni <alqorni@Ms-MacBook-Pro-2.local>
2024-10-12 15:29:54 +0700

    membuat desain register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syn_laundry/pages/login_page.dart';
+import 'package:syn_laundry/pages/register_page.dart';
 import 'package:syn_laundry/pages/splash_page.dart';
 
 void main() {
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: RegisterPage(),
     );
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:syn_laundry/themes/themes.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // baris 1 : AppBar
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Persiapkan Akunmu",
+          style: primaryTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        leading: Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Image.asset('assets/ic-back.png')),
+      ),
       body: ListView(
         children: [
-          // baris 1 : IMAGE
-          Image.asset('assets/img-login.png'),
-          SizedBox(
-            height: 37,
-          ),
-          // baris 2 : TEXT
-          Center(
-            child: Text(
-              "Laundry Lebih Mudah!",
-              style: primaryTextStyle.copyWith(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          // baris 3 : KOLOM INPUT
+          // baris 2 : KOLOM INPUT
           Container(
             margin: EdgeInsets.only(top: 60, left: 20, right: 20),
             child: Column(
@@ -32,9 +31,55 @@ class LoginPage extends StatelessWidget {
               children: [
                 TextFormField(
                   // maxLines: 4,
-                  keyboardType: TextInputType.emailAddress,
+                  keyboardType: TextInputType.text,
                   decoration: InputDecoration(
                     hintText: "Masukkan Username",
+                    hintStyle: secondaryTextStyle,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 37,
+                ),
+                TextFormField(
+                  // maxLines: 4,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    hintText: "Masukkan Nomer Telepon",
+                    hintStyle: secondaryTextStyle,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 37,
+                ),
+                TextFormField(
+                  // maxLines: 4,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: "Masukkan Email",
                     hintStyle: secondaryTextStyle,
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
@@ -57,7 +102,10 @@ class LoginPage extends StatelessWidget {
                   obscureText: true,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
-                    suffix: Image.asset('assets/ic-eye.png', width: 20,),
+                    suffix: Image.asset(
+                      'assets/ic-eye.png',
+                      width: 20,
+                    ),
                     hintText: "Masukkan Password",
                     hintStyle: secondaryTextStyle,
                     enabledBorder: OutlineInputBorder(
@@ -77,20 +125,14 @@ class LoginPage extends StatelessWidget {
                 SizedBox(
                   height: 10,
                 ),
-                Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      "Lupa Password",
-                      style: greenTextStyle.copyWith(
-                        fontSize: 16,
-                      ),
-                    )),
                 SizedBox(
                   height: 37,
                 ),
+                
                 Container(
                   height: 50,
                   width: double.infinity,
+                  
                   child: TextButton(
                     style: TextButton.styleFrom(
                       backgroundColor: Color(0xFF4ABF92),
@@ -100,7 +142,7 @@ class LoginPage extends StatelessWidget {
                     ),
                     onPressed: null,
                     child: Text(
-                      "Masuk",
+                      "Daftar",
                       style: TextStyle(
                         color: Colors.white,
                       ),
@@ -116,14 +158,14 @@ class LoginPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Belum punya akun?",
+                "Sudah punya akun?",
                 style: secondaryTextStyle.copyWith(fontSize: 16),
               ),
               SizedBox(
                 width: 2,
               ),
               Text(
-                "Daftar",
+                "Login",
                 style: greenTextStyle.copyWith(
                   fontSize: 16,
                 ),
