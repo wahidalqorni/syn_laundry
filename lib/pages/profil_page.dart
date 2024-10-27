9ae8fb2b87d3c7dd62b9b401b74a3602771b810c
wahidalqorni <idunalqorni@gmail.com>
2024-10-27 08:01:41 +0700

    update pop up logout

--- lib/pages/profil_page.dart
+++ lib/pages/profil_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syn_laundry/pages/splash_page.dart';
 import 'package:syn_laundry/themes/themes.dart';
 
 class ProfilPage extends StatelessWidget {
@@ -27,62 +28,153 @@ class ProfilPage extends StatelessWidget {
                 style: greenTextStyle,
               ),
             ),
-
-            SizedBox(height: 41,),
-
-            Text("Username", style: primaryTextStyle.copyWith(
-              fontWeight: FontWeight.w500,
-            ), ),
-
-            SizedBox(height: 14,),
-
-            Text("Uzumaki Naruto", style: primaryTextStyle.copyWith(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ), ),
-
-            SizedBox(height: 14,),
-
-            Text("Email", style: primaryTextStyle.copyWith(
-              fontWeight: FontWeight.w500,
-            ), ),
-
-            SizedBox(height: 14,),
-
-            Text("naruto@uzumaki.com", style: primaryTextStyle.copyWith(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ), ),
-
-            SizedBox(height: 14,),
-
-            Text("Nomer Telepon", style: primaryTextStyle.copyWith(
-              fontWeight: FontWeight.w500,
-            ), ),
-
-            SizedBox(height: 14,),
-
-            Text("08992121212", style: primaryTextStyle.copyWith(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ), ),
-
-            SizedBox(height: 39,),
-
+            SizedBox(
+              height: 41,
+            ),
+            Text(
+              "Username",
+              style: primaryTextStyle.copyWith(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Uzumaki Naruto",
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Email",
+              style: primaryTextStyle.copyWith(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              "naruto@uzumaki.com",
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Nomer Telepon",
+              style: primaryTextStyle.copyWith(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              "08992121212",
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 39,
+            ),
             Row(
               children: [
-                Text("Reset Password", style: primaryTextStyle.copyWith(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),),
+                Text(
+                  "Reset Password",
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
                 Spacer(),
-                Icon(Icons.arrow_forward_ios, size: 24,),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 24,
+                ),
               ],
             ),
-
-            SizedBox(height: 39,),
-
-            Text("Logout", style: redTextStyle.copyWith(fontSize: 16), ),
+            SizedBox(
+              height: 39,
+            ),
+            GestureDetector(
+                onTap: () {
+                  // memnunculkan Alert
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Expanded(
+                        child: AlertDialog(
+                          //title: const Text('Button Clicked!'),
+                          content: Text(
+                            'Yakin ingin keluar akun?',
+                            style: primaryTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          actions: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(
+                                      bottom: 24, left: 20, right: 20),
+                                  width: double.infinity,
+                                  height: 50,
+                                  child: TextButton(
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: Color(0xFF4ABF92),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      "Batalkan",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            GestureDetector(
+                              onTap: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => SplashPage() ));
+                              },
+                              child: Center(
+                                child: Text("Yakin, keluar", style: redTextStyle.copyWith(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ), ),
+                              ),
+                            ),
+                            
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Text(
+                  "Logout",
+                  style: redTextStyle.copyWith(fontSize: 16),
+                )),
           ],
         ),
       ),
